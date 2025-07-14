b0440084736ad0b5c86d6cab97fe2fdc96277622
ektateli <ektateli1911@gmail.comm>
2025-07-14 16:53:13 +0530

    Initial

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,22 @@
 // Backend/config/db.js
 const mysql = require('mysql2');
+require('dotenv').config();
+
+
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root', // change if your MySQL username is different
+//   password: 'admin', // change if your MySQL password is not empty
+//   database: 'home_rentals' // change if your DB name is different
+// });
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // change if your MySQL username is different
-  password: 'admin', // change if your MySQL password is not empty
-  database: 'home_rentals' // change if your DB name is different
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
+
 
 db.connect((err) => {
   if (err) {
